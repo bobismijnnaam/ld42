80300f238322d69e93f7bec5fbe0cbd3e0390b7e
bobismijnnaam <bobrubbens@gmail.com>
2018-08-11 10:36:35 +0200

    Can now zoom in on the game screen

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player : MonoBehaviour {
+
+    public GameObject gameScreenPlane;
+    public Camera screenCamera;
+
+    public FirstPersonController fpController;
+    public Camera mainCamera;
+
+    private PlayerMode playerMode = PlayerMode.WALKING;
+
+    enum PlayerMode {
+        WALKING,
+        PLAYING
+    }
+
+	// Use this for initialization
+	void Start () {
+        fpController = GetComponent<FirstPersonController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        var wasHit = Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, 2f);
+        if (wasHit && hit.collider.gameObject == gameScreenPlane && Input.GetButtonDown("Fire1")) {
+            switchMode();
+        }
+	}
+
+    void switchMode() {
+        if (playerMode == PlayerMode.WALKING) {
+            mainCamera.enabled = false;
+            screenCamera.enabled = true;
+            fpController.enabled = false;
+            playerMode = PlayerMode.PLAYING;
+        } else {
+            mainCamera.enabled = true;
+            screenCamera.enabled = false;
+            fpController.enabled = true;
+            playerMode = PlayerMode.WALKING;
+        }
+     }   
+}
