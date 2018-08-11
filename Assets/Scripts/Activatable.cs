b46feb92acb6ef62ebc683fa496da8966d297086
bobismijnnaam <bobrubbens@gmail.com>
2018-08-11 15:28:16 +0200

    Drawer components!

---
+++ Assets/Scripts/Activatable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Activatable : MonoBehaviour {
+    public abstract string getDescription();
+
+    public abstract void activate();
+}
---
+++ Assets/Scripts/DrawerComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerComponent : Activatable {
+
+    enum State {
+        OPEN,
+        CLOSED
+    }
+
+    private State state = State.CLOSED;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override string getDescription() {
+        if (state == State.OPEN) {
+            return "close";
+        } else {
+            return "open";
+        }
+    }
+
+    public override void activate() {
+        if (state == State.OPEN) {
+            gameObject.transform.Translate(10, 0, 0);
+            state = State.CLOSED;
+        } else if (state == State.CLOSED) {
+            gameObject.transform.Translate(-10, 0, 0);
+            state = State.OPEN;
+        }
+    }
+}
