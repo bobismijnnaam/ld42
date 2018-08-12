cbbb7dd8c49aec267cdff828aa3c095a49773e1a
bobismijnnaam <bobrubbens@gmail.com>
2018-08-12 13:14:34 +0200

    Locked door

---
+++ Assets/Scripts/SoundOnActivateComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnActivateComponent : Activatable {
+
+    public string description;
+    public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override string getDescription() {
+        return description;
+    }
+
+    public override void activate() {
+        audioSource.Play();
+    }
+}
